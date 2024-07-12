694b221ad3ede378760f5940a67070ac054788e0
nicorey89 <nicolasthian8907@gmail.com>
2024-07-11 21:07:00 -0300

    agrego la carpeta .env.example

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const process = require("process");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 
 app.use(express.static("public"));
